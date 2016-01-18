5746415c1bd81c8221f77d9070e1fa9388763bc9
5cript <tim06tr@gmail.com>
2016-01-18 14:33:19 +0100

    Added fromMarkup and toMarkup implementations.

---
+++ components/comments.cpp
@@ -0,0 +1,24 @@
+#include "comments.hpp"
+
+#include "parsers/parse.hpp"
+#include "parsers/comments.hpp"
+
+namespace WikiMarkup { namespace Components {
+
+    std::string CommentText::toMarkup()
+    {
+        return std::string("<!--") + data + "-->";
+    }
+    ParsingResult CommentText::fromMarkup(std::string const& mu)
+    {
+        using namespace WikiMarkup::Components::Parser;
+
+        TYPEDEF_GRAMMAR(comment_grammar);
+
+        auto res = parse <grammar> (mu);
+        *this = res.second;
+        return res.first;
+    }
+
+} // namespace Components
+} // namespace WikiMarkup
