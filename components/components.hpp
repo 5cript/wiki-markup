61a02c82d9f2f74dad7c157a3194067ae9ceec3c
5cript <tim06tr@gmail.com>
2016-01-18 14:34:16 +0100

    Added convenience header that includes all the components.

---
+++ components/components.hpp
@@ -0,0 +1,13 @@
+#ifndef COMPONENTS_COMPONENTS_HPP_INCLUDED
+#define COMPONENTS_COMPONENTS_HPP_INCLUDED
+
+#include "component.hpp"
+#include "comments.hpp"
+#include "format.hpp"
+#include "header.hpp"
+#include "link.hpp"
+#include "list.hpp"
+#include "table.hpp"
+#include "text.hpp"
+
+#endif // COMPONENTS_COMPONENTS_HPP_INCLUDED
