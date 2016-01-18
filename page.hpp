159f0ad617f9f96ffb7c627bf9b1d155b4d50d4a
5cript <tim06tr@gmail.com>
2016-01-18 14:32:32 +0100

    Can now add components to a page.

--- page.hpp
+++ page.hpp
@@ -1,14 +1,26 @@
 #ifndef PAGE_HPP_INCLUDED
 #define PAGE_HPP_INCLUDED
 
+#include "components/component.hpp"
+
 #include <vector>
+#include <memory>
 
 namespace WikiMarkup
 {
     class Page
     {
+    public:
+        Page() = default;
+        ~Page() = default;
+
+        template <typename T>
+        void appendComponent(T const& comp) {
+            components_.emplace_back (new T(comp));
+        }
+
     private:
-        std::vector components_;
+        std::vector <std::unique_ptr <Components::IComponent>> components_;
     };
 }
 
