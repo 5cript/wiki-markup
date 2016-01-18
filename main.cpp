632bd620f5c931dd8ffb7c1469b134bd4730096e
5cript <tim06tr@gmail.com>
2016-01-18 14:32:12 +0100

    Removed test code. Added test code for PageParser.

--- main.cpp
+++ main.cpp
@@ -3,9 +3,11 @@
 //#include "components/header.hpp"
 //#include "components/list.hpp"
 
-#include "components/parsers/parse.hpp"
-#include "components/parsers/url.hpp"
-#include "components/parsers/link.hpp"
+//#include "components/parsers/parse.hpp"
+//#include "components/parsers/url.hpp"
+//#include "components/parsers/link.hpp"
+
+#include "parser/page_parser.hpp"
 
 
 #include <iostream>
@@ -19,42 +21,11 @@
 int main()
 {
     using namespace WikiMarkup;
-    using namespace Components;
 
     auto data = readStringFromFile ("testfile.txt");
 
-    using namespace WikiMarkup::Components::Parser;
-
-    TYPEDEF_GRAMMAR(link_grammar);
-
-    try {
-        auto result = parse <grammar> (data, true, true);
-
-        std::cout << "internal: " << result.internal << "\n";
-        std::cout << "colon: " << result.colon << "\n";
-        std::cout << "anchor: " << result.anchor << "\n";
-        std::cout << "prefix: " << result.prefix << "\n";
-        std::cout << "localPart: " << result.localPart << "\n";
-        std::cout << "attributes: \n";
-        for (auto const& i : result.attributes)
-            std::cout << "\t" << i << "\n";
-        std::cout << "redirect: " << result.redirect << "\n";
-
-
-        std::cout << "url.scheme: " << result.url.scheme << "\n";
-        std::cout << "url.authority.user: " << result.url.authority.user << "\n";
-        std::cout << "url.authority.password: " << result.url.authority.password << "\n";
-        std::cout << "url.authority.host: " << result.url.authority.host << "\n";
-        std::cout << "url.authority.port: " << result.url.authority.port << "\n";
-        std::cout << "url.path: ";
-        for (auto const& i : result.url.path)
-             std::cout << i << "/";
-        std::cout << "\n";
-        std::cout << "url.query: " << result.url.query << "\n";
-        std::cout << "url.fragment: " << result.url.fragment << "\n";
-    } catch (std::exception const& exc) {
-        std::cout << exc.what() << "\n";
-    }
+    PageParser parser (data);
+    parser.parse();
 
     return 0;
 }
