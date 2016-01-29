5bf3252f102e05e892db0b80597b6fc662ccac8a
5cript <tim06tr@gmail.com>
2016-01-29 16:49:18 +0100

    Markup for links can now be generated.

--- components/link.cpp
+++ components/link.cpp
@@ -3,11 +3,66 @@
 #include "parsers/parse.hpp"
 #include "parsers/link.hpp"
 
+#include "post_processors/link.hpp"
+
 namespace WikiMarkup { namespace Components {
 //####################################################################################
     std::string Link::toMarkup()
     {
-        return "link";
+        std::stringstream sstr;
+
+        std::cout << url.scheme << "\n";
+        std::cout << url.authority.host << "\n";
+
+        auto appendAttributes = [&, this]() {
+            if (!attributes.empty())
+            {
+                sstr << '|';
+                sstr << attributes.front();
+            }
+        };
+
+        auto printRedirect = [&, this]() {
+            if (redirect)
+                sstr << "#REDIRECT ";
+        };
+
+        if (internal)
+        {
+            // anchor detection is not useful the way it was before.
+            // no parsing, therefor no generation.
+
+            printRedirect();
+
+            sstr << "[[" << prefix;
+
+            if (colon)
+            {
+                sstr << ':';
+                sstr << localPart;
+            }
+
+            appendAttributes();
+
+            sstr << "]]";
+        }
+        else if (!implicit)
+        {
+            printRedirect();
+
+            sstr << '[';
+            sstr << url.toString();
+
+            appendAttributes();
+
+            sstr << ']';
+        }
+        else
+        {
+            sstr << url.toString();
+        }
+
+        return sstr.str();
     }
 //-----------------------------------------------------------------------------------
     ParsingResult Link::fromMarkup(std::string const& mu)
@@ -18,6 +73,7 @@ namespace WikiMarkup { namespace Components {
 
         auto res = parse <grammar> (mu);
         *this = res.second;
+        PostProcessors::postProcessLink(*this);
         return res.first;
     }
 //-----------------------------------------------------------------------------------
